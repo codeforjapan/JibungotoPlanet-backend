80a11960772f13712563a094dee0c79149c53787
naoji3x <naoji.3x@gmail.com>
2022-07-12 22:49:21 +0900

    test staging

--- test/curl/put-housing.sh
+++ test/curl/put-housing.sh
@@ -1,1 +1,1 @@
-curl -X PUT -H "Content-Type: application/json" -d '@put-housing.json' ${JIBUNGOTO_PLANET_REST}/profiles/c9cb3a15-d093-4040-82fb-eacbcd090348 > put-housing.result.json
+curl -X PUT -H "Content-Type: application/json" -d '@put-housing.json' ${JIBUNGOTO_PLANET_REST}/profiles/89f5361d-3b10-4991-b750-d42b9bd1ad81 > put-housing.result.json
